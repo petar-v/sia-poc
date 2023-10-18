8b6b59d3044db59d377ad3a1113c8da8665ebb82
Petar Vasilev <petar@pshow.org>
2023-10-19 00:38:14 +0100

    layouts fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Header from "./components/header";
 import App from "../sia/App";
 
 export default function Home() {
-  const apiKey = process.env.CHATGPT_API_KEY || "";
-  return (
-    <>
-      <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <App ApiKey={apiKey}></App>
-      </main>
-    </>
-  );
+    const apiKey = process.env.OPENAI_API_KEY || "";
+    return (
+        <>
+            <Header />
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <App ApiKey={apiKey}></App>
+            </main>
+        </>
+    );
 }
