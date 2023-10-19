67a711629a128cd12843c4f5d806fb101292f50f
Petar Vasilev <petar@pshow.org>
2023-10-19 09:28:28 +0100

    add modals

--- src/sia/components/projectPlan.tsx
+++ src/sia/components/projectPlan.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ProjectData from "../projectData";
 
-import { Card, Space, Descriptions } from "antd";
+import { Card, Space, Descriptions, Modal } from "antd";
 import type { DescriptionsProps } from "antd";
 
 export type ProjectPlanProps = {
@@ -9,6 +9,8 @@ export type ProjectPlanProps = {
 };
 
 export default function ProjectPlan({ data }: ProjectPlanProps) {
+    const [taskOpen, setTaskOpen] = useState(-1);
+
     const items: DescriptionsProps["items"] = [
         {
             key: "1",
@@ -26,19 +28,35 @@ export default function ProjectPlan({ data }: ProjectPlanProps) {
             children: data.numberOfEngineers,
         },
     ];
+    const modalHandleClose = () => {
+        setTaskOpen(-1);
+    };
     return (
         <>
-            <Descriptions title="Project Details" items={items} />;
+            <Descriptions title="Project Details" items={items} />
             <Space className="flex flex-wrap">
                 {data.tasks.map((task, i) => (
-                    <Card
-                        key={`task-${i}`}
-                        title={task.title}
-                        extra={<small>{task.timeEst} days</small>}
-                        style={{ width: 300 }}
-                    >
-                        <p>{task.description}</p>
-                    </Card>
+                    <>
+                        <Card
+                            hoverable
+                            key={`task-${i}`}
+                            title={task.title}
+                            extra={<small>{task.timeEst} days</small>}
+                            style={{ width: 300 }}
+                            onClick={() => setTaskOpen(i)}
+                        >
+                            <p>{task.description}</p>
+                        </Card>
+                        <Modal
+                            title={task.title}
+                            footer={null}
+                            open={taskOpen === i}
+                            onOk={modalHandleClose}
+                            onCancel={modalHandleClose}
+                        >
+                            <p>{task.description}</p>
+                        </Modal>
+                    </>
                 ))}
             </Space>
         </>
