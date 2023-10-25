4b3af0eba3c70363f205ac62c4f8902687a5e034
Petar Vasilev <petar@pshow.org>
2023-10-25 13:48:10 +0100

    fixed configs for static build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 const nextConfig = {
+    distDir: "build",
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")],
     },
